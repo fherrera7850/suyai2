6d58b376ae8c1054896b813aa6569af291392e95
Felipe Herrera <fherrera7850@gmail.com>
2024-01-30 09:58:24 -0300

    productos desde bd, se quita context

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,46 +1,51 @@
-import React, { useContext } from "react";
-import { ShopContext } from "../../context/shop-context";
-import { PRODUCTS } from "../../products";
+import React from "react";
 import { CartItem } from "./cart-item";
 import { useNavigate } from "react-router-dom";
 
 import "./cart.css";
+
 export const Cart = () => {
-  const { cartItems, getTotalCartAmount, checkout } = useContext(ShopContext);
-  const totalAmount = getTotalCartAmount();
+  //const totalAmount = getTotalCartAmount();
 
   const navigate = useNavigate();
+
+
 
   return (
     <div className="cart">
       <div>
-        <h1 style={{color: "darkslategray"}}>Carrito de compras</h1>
+        <h1 style={{ color: "darkslategray" }}>Carrito de compras</h1>
       </div>
       <div className="cart">
-        {PRODUCTS.map((product) => {
+        {/* {PRODUCTS.map((product) => {
           if (cartItems[product.id] !== 0) {
-            return <CartItem data={product} />;
+            return <CartItem data={product} key={product.id} />;
           }
-        })}
+        })} */}
       </div>
-
-      {totalAmount > 0 ? (
+  
+      {/* totalAmount > 0 ? ( */}
         <div className="checkout">
-          <p> Subtotal: ${totalAmount} </p>
-          <button onClick={() => navigate("/")}> Continuar comprando </button>
+          {/* <p style={{ fontSize: "1.5rem", marginBottom: "1rem", textAlign: "left" }}>
+            Subtotal: <b>${formatSubtotal(totalAmount)}</b>
+          </p>
+          <button onClick={() => navigate("/")}>Continuar comprando</button>}
           <button
             onClick={() => {
               checkout();
               navigate("/checkout");
             }}
+            style={{fontSize:15}}
           >
-            {" "}
-            Pagar y/o agendar pedido{" "}
-          </button>
+            Completar pedido
+          </button> */}
         </div>
-      ) : (
-        <h1 style={{color: "darkslategray"}}> Tu carrito de compra esta vacio</h1>
-      )}
+      {/* ) : (
+        <h1 style={{ color: "darkslategray" }}>
+          Tu carrito de compra está vacío
+        </h1>
+      ) */}
     </div>
   );
+  
 };
